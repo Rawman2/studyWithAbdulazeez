178004496fc2bf204c63ac2133f6a544218ceded
Rawman <rouffaderemi@gmail.com>
2022-04-20 16:42:23 +0100

    init

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
-
-console.log("5" - "1");
+let counter = 0;
+for (let currentNumber = 16; currentNumber += 1;) {
+    if (currentNumber % 4 == 0 && counter == 2) {
+        console.log(currentNumber);
+       counter += 1
+    }
+}
