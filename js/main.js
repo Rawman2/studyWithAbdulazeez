8092f0c5174c3a3b074fd43c88d71ff7e0c4500a
Rawman <rouffaderemi@gmail.com>
2022-04-20 16:46:51 +0100

    git commit -m 'lesson 1'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-let counter = 0;
-for (let currentNumber = 16; currentNumber += 1;) {
-    if (currentNumber % 4 == 0 && counter == 2) {
-        console.log(currentNumber);
-       counter += 1
+// let hashes = "#";
+// let counter = 0;
+// while (counter <= 6) {
+//     console.log(hashes);
+//     counter++;
+//     hashes += "#";
+// }
+
+// for (let hashes = "#"; hashes.length <= 6; hashes += "#")
+// console.log(hashes);
+
+// for (let numbers = 0; numbers <= 100; numbers++) {
+//     let specialCases = "";
+//     if (numbers % 3 == 0) specialCases += "Fizz";
+//     if (numbers % 5 == 0) specialCases += "Buzz";
+//     console.log(specialCases || numbers);
+// }
+
+function greet(who) {
+    console.log("Hello " + who);
     }
-}
+    greet("Harry");
+    console.log("Bye");
